5e45b9c4b105e10af4ea42f4d12f0b43c7c5ddb5
dagilleland <dagilleland@shaw.ca>
2015-12-01 06:54:31 -0700

    Admin page (GridViews fro Users/Roles)

---
+++ eRestaurant Demo/Website/Admin/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Admin_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <div class="jumbotron">
+        <h1>Security Administration</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Users</h2>
+            <asp:GridView ID="SiteUsers" runat="server"></asp:GridView>
+        </div>
+        <div class="col-md-6">
+            <h2>Roles</h2>
+            <asp:GridView ID="SiteRoles" runat="server"></asp:GridView>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ eRestaurant Demo/Website/Admin/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Website;
+
+public partial class Admin_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            PopulateUserInfo();
+            PopulateRoleInfo();
+        }
+    }
+
+    private void PopulateUserInfo()
+    {
+        UserManager mgr = new UserManager();
+        SiteUsers.DataSource = mgr.Users.ToList();
+        SiteUsers.DataBind();
+    }
+
+    private void PopulateRoleInfo()
+    {
+        //RoleManager<IdentityRole> mgr = new RoleManager<IdentityRole>(;
+        //SiteRoles.DataSource = mgr.Roles.ToList();
+        //SiteRoles.DataBind();
+    }
+}
